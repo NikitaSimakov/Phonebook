b8da5b549f719df7df6e68b336db2cd456724004
NikitaSimakov <mishaaniskin@gmail.com>
2023-07-20 16:43:30 +0300

    Update Registration.jsx

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -2,7 +2,6 @@ import { Notify } from 'notiflix';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, TextField } from '@mui/material';
-
 import css from './Registration.module.css';
 import CircularIndeterminate from 'components/CircularProgress/CircularProgress';
 import { useDispatch, useSelector } from 'react-redux';
