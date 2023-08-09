a48c29b8e9b745f533f29f495e620414262b77b4
NikitaSimakov <mishaaniskin@gmail.com>
2023-08-09 21:32:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
-
 import App from 'components/App/App';
-
 import { Provider } from 'react-redux';
-
 import './index.css';
 import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
