ef822be750b2040ff97f63fd86cca9ad2853824a
NikitaSimakov <mishaaniskin@gmail.com>
2023-08-11 20:53:34 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { reducer } from './reducer';
+
 import persistStore from 'redux-persist/es/persistStore';
 
 export const store = configureStore({
